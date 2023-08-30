20b150e33db8029677f64625de165959c8393a27
GhoshM <mainakgh1@gmail.com>
2023-08-31 02:57:26 +0530

    Maven-1 notes

---
+++ Maven-1/maven-test/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Gson g = new Gson();
+    }
+}
