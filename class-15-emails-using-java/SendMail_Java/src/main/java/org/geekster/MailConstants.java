9d17a28e3b6af3b90deab9d12fe854c584e83a77
GhoshM <mainakgh1@gmail.com>
2023-09-04 22:15:10 +0530

    email using java

---
+++ class-15-emails-using-java/SendMail_Java/src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,7 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "mainakgh1@gmail.com";
+    public static final String APP_PASSWORD = "hkbkhbfvg" ;
+}
---
+++ class-15-emails-using-java/SendMail_Java/src/main/java/org/geekster/MailHandlerBase.java
@@ -0,0 +1,67 @@
+package org.geekster;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class MailHandlerBase {
+
+    public void sendMail()
+    {
+
+        //properties
+
+        //something like a map
+        Properties sysPropertiesMap = System.getProperties();
+
+        //System.out.println(sysPropertiesMap);
+
+        //in this map I want to add 4 more key value pairs : mailing
+
+        sysPropertiesMap.put("mail.smtp.host","smtp.gmail.com"); //smtp server
+        sysPropertiesMap.put("mail.smtp.port","465"); //server port
+        sysPropertiesMap.put("mail.smtp.ssl.enable","true"); //ssl -secure socket layer
+        sysPropertiesMap.put("mail.smtp.auth","true"); //authentication - auth
+
+
+        //authentication
+
+        Authenticator mailAuthenticator = new CustomizedMailAuthenticator();
+
+
+        //session
+        Session mailSession = Session.getInstance(sysPropertiesMap,mailAuthenticator);
+
+
+        //build the mail
+        //Mime message
+
+        MimeMessage mailMessage = new MimeMessage(mailSession);
+
+        try {
+            mailMessage.setFrom(MailConstants.SENDER);
+            mailMessage.setSubject("Geekster Mailing class");
+            mailMessage.setRecipient(Message.RecipientType.TO, new InternetAddress("nishantbudia@gmail.com"));
+            mailMessage.setText("Mainak is trying to teach mailing through java!!!");
+
+            Transport.send(mailMessage);
+
+            System.out.println("Mail sent !!!");
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Some error while preparing mail body!!!!");
+            System.out.println(ex.getMessage());
+        }
+
+
+
+
+
+
+    }
+}
