8b6d4db3a9809456d775cd1d8994f5e676b1f319
GhoshM <mainakgh1@gmail.com>
2023-10-20 08:30:31 +0530

    aws-class-3

--- class-28-roomAPP-H2/Room-APP/src/main/java/com/geekster/Room/APP/controller/RoomController.java
+++ class-28-roomAPP-H2/Room-APP/src/main/java/com/geekster/Room/APP/controller/RoomController.java
@@ -35,4 +35,11 @@ public class RoomController {
         return roomService.getRooms();
     }
 
+
+    @GetMapping("Mainak")
+    public String getTest()
+    {
+        return "Mainak";
+    }
+
 }
